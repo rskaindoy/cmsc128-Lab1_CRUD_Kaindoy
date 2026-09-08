7ed23745affd91e239c91a0412ea9c306ac414db
rskaindoy <rskaindoy@up.edu.ph>
2026-09-09 02:16:35 +0800

    feat: working create task feature with basic ui

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,57 @@
 console.log("Lista app loaded!");
 
-// TODO: store API URL
+// store API URL
 const API_URL = "http://localhost:3000/api";
+
+// get HTML elements
+const addTaskButton = document.getElementById("add-task-button");
+const closeTaskPanel = document.getElementById("close-task-panel");
+const addTaskPanel = document.getElementById("add-task-panel");
+const taskForm = document.getElementById("task-form");
+
+// open the add-task panel
+addTaskButton.addEventListener("click", () => {
+    addTaskPanel.style.display = "block";
+});
+
+// close the add-task panel
+closeTaskPanel.addEventListener("click", () => {
+    addTaskPanel.style.display = "none";
+});
+
+// submit filled-out task form
+taskForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    // get values from the form
+    const title = document.getElementById("title").value;
+    const due = document.getElementById("due").value;
+    const priority = document.getElementById("prio").value;
+    const tag = document.getElementById("tag").value;
+
+    // send task to backend
+    const response = await fetch(`${API_URL}/tasks`, {
+        method: "POST",
+        headers:{"Content-Type": "application/json"},
+        body: JSON.stringify({
+            title: title,
+            due: due,
+            priority: priority,
+            tag: tag
+        })
+    });
+
+    // cgeck if task was created successfully
+    if (response.ok){
+        console.log("Task created");
+
+        // clear the form and close the panel
+        taskForm.reset();
+        addTaskPanel.style.display = "none";
+    } else {
+        console.error("Failed to create task.")
+    }
+});
 
 // TODO: create func to fetch tasks
 // async function loadTasks() {}
